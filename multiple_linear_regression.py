d7d96de69df9f0c3c6810813ae8725257bf9b880
Krish C Naik <krishnaik06@gmail.com>
2019-01-31 21:48:34 +0530

    Add files via upload

---
+++ multiple_linear_regression.py
@@ -0,0 +1,38 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, 4]
+
+#Convert the column into categorical columns
+
+states=pd.get_dummies(X['State'],drop_first=True)
+
+# Drop the state coulmn
+X=X.drop('State',axis=1)
+
+# concat the dummy variables
+X=pd.concat([X,states],axis=1)
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+from sklearn.metrics import r2_score
+score=r2_score(y_test,y_pred)
